b02694cd97115294bff06462e449262d843f5db4
Willyan Braga <willyan.braga00@gmail.com>
2022-12-08 14:15:27 -0300

    asd

--- src/app/Pages/auth/login/login.component.ts
+++ src/app/Pages/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../resources/services/login.service';
+import { RequestLogin } from '../resources/models/RequestLogin';
+
 
 @Component({
   selector: 'app-login',
@@ -6,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public requestLogin: RequestLogin = new RequestLogin;
 
-  constructor() { }
+  constructor(private loginservice: LoginService) { }
   ngOnInit(): void {
+    this.requestLogin = new RequestLogin();
+  }
+
+  public doLogin() :void{
+    this.loginservice.doLogin(this.requestLogin).subscribe(data => {
+      console.log(data);
+    },
+    (error) => {
+      console.error(error);
+    }
+    );
   }
 
 }
--- src/app/Pages/auth/login/login.module.ts
+++ src/app/Pages/auth/login/login.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -9,7 +11,9 @@ import { LoginComponent } from './login.component';
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ]
 })
 export class LoginModule { }
